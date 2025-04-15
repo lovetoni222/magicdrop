7f161170f615d93435d4d211f724f52c8bc2243c
lovetoni222 <toni@getmagicdrop.com>
2025-04-15 11:47:56 -0700

    Safelist sparkle-layer class for Tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./app/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: ['sparkle-layer'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
